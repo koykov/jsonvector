0e39cd05f84dd9cb1812c4f7ac29406e3858f25b
Konstantin Voykov <koykov0@gmail.com>
2020-08-30 23:03:22 +0300

    Comment byteptr code.

--- byteptr.go
+++ byteptr.go
@@ -7,19 +7,26 @@ import (
 	"github.com/koykov/fastconv"
 )
 
+// Byte sequence.
 type byteptr struct {
+	// Offset in virtual memory.
 	o uint64
+	// Length of bytes array.
 	l int
+	// Escaped byte sequence.
 	e bool
 }
 
+// Set new offset and length.
 func (m *byteptr) set(o uint64, l int) {
 	m.o, m.l = o, l
 }
 
+// Convert byte sequence to byte array.
 func (m *byteptr) Bytes() []byte {
 	p := m.rawBytes()
 	if m.e {
+		// Unescape byte array.
 		p = unescape(p)
 		m.l = len(p)
 		m.e = false
@@ -27,10 +34,12 @@ func (m *byteptr) Bytes() []byte {
 	return p
 }
 
+// Convert byte sequence to string.
 func (m *byteptr) String() string {
 	return fastconv.B2S(m.Bytes())
 }
 
+// Convert byte sequence to byte array.
 func (m *byteptr) rawBytes() []byte {
 	h := reflect.SliceHeader{
 		Data: uintptr(m.o),
