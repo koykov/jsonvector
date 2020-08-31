7d23d4fe0d455b230b19a36003b3fea62a88a463
Konstantin Voykov <koykov0@gmail.com>
2020-08-31 23:45:17 +0300

    Comment parser code.

--- parser.go
+++ parser.go
@@ -8,6 +8,7 @@ import (
 	"github.com/koykov/bytealg"
 )
 
+// Main internal parser helper.
 func (vec *Vector) parse(s []byte, copy bool) (err error) {
 	if len(s) == 0 {
 		err = ErrEmptySrc
@@ -15,24 +16,33 @@ func (vec *Vector) parse(s []byte, copy bool) (err error) {
 	}
 	s = bytealg.Trim(s, bFmt)
 	if copy {
+		// Copy input data.
 		vec.s = append(vec.s[:0], s...)
 	} else {
+		// Use input data as source.
 		vec.s = s
 	}
+
+	// Get source data address and raw parser pointer.
 	h := (*reflect.SliceHeader)(unsafe.Pointer(&vec.s))
 	vec.a = uint64(h.Data)
 	vec.p = vec.ptr()
 
 	offset := 0
+	// Create root node and register it.
 	val := vec.newNode(0)
 	i := vec.l - 1
 	vec.r.reg(0, i)
+
+	// Parse source data.
 	offset, err = vec.parseGeneric(0, offset, val)
 	if err != nil {
 		vec.e = offset
 		return err
 	}
 	vec.v[i] = *val
+
+	// Check unparsed tail.
 	if offset < len(vec.s) {
 		vec.e = offset
 		return ErrUnparsedTail
@@ -41,10 +51,12 @@ func (vec *Vector) parse(s []byte, copy bool) (err error) {
 	return
 }
 
+// Generic parser helper.
 func (vec *Vector) parseGeneric(depth, offset int, v *Node) (int, error) {
 	var err error
 	switch {
 	case vec.s[offset] == 'n':
+		// Check null node.
 		if len(vec.s[offset:]) > 3 && bytes.Equal(bNull, vec.s[offset:offset+4]) {
 			v.t = TypeNull
 			offset += 4
@@ -52,25 +64,33 @@ func (vec *Vector) parseGeneric(depth, offset int, v *Node) (int, error) {
 			return offset, ErrUnexpId
 		}
 	case vec.s[offset] == '{':
+		// Check open object node.
 		v.t = TypeObj
 		offset, err = vec.parseObj(depth+1, offset, v)
 	case vec.s[offset] == '[':
+		// Check open array node.
 		v.t = TypeArr
 		offset, err = vec.parseArr(depth+1, offset, v)
 	case vec.s[offset] == ']':
+		// Check end array node.
 		return offset, ErrEOA
 	case vec.s[offset] == '"':
+		// Check string node.
 		v.t = TypeStr
+		// Save offset of string value.
 		v.v.o = vec.a + uint64(offset+1)
+		// Get index of end of string value.
 		e := bytealg.IndexAt(vec.s, bQuote, offset+1)
 		if e < 0 {
 			return len(vec.s), ErrUnexpEOS
 		}
 		v.v.e = false
 		if vec.s[e-1] != '\\' {
+			// Good case - string isn't escaped.
 			v.v.l = e - offset - 1
 			offset = e + 1
 		} else {
+			// Walk over double quotas and look for unescaped.
 			_ = vec.s[len(vec.s)-1]
 			for i := e; i < len(vec.s); {
 				i = bytealg.IndexAt(vec.s, bQuote, i+1)
@@ -88,10 +108,13 @@ func (vec *Vector) parseGeneric(depth, offset int, v *Node) (int, error) {
 			offset = e + 1
 		}
 		if !v.v.e {
+			// Extra check of escaping sequences.
 			v.v.e = bytes.IndexByte(v.v.rawBytes(), '\\') >= 0
 		}
 	case isDigit(vec.s[offset]):
+		// Check number node.
 		if len(vec.s[offset:]) > 0 {
+			// Get the edges of number.
 			i := offset
 			for isDigitDot(vec.s[i]) {
 				i++
@@ -107,6 +130,7 @@ func (vec *Vector) parseGeneric(depth, offset int, v *Node) (int, error) {
 			return offset, ErrUnexpEOF
 		}
 	case vec.s[offset] == 't':
+		// Check bool (true) node.
 		if len(vec.s[offset:]) > 3 && bytes.Equal(bTrue, vec.s[offset:offset+4]) {
 			v.t = TypeBool
 			v.v.set(vec.a+uint64(offset), 4)
@@ -115,6 +139,7 @@ func (vec *Vector) parseGeneric(depth, offset int, v *Node) (int, error) {
 			return offset, ErrUnexpId
 		}
 	case vec.s[offset] == 'f':
+		// Check bool (false) node.
 		if len(vec.s[offset:]) > 4 && bytes.Equal(bFalse, vec.s[offset:offset+5]) {
 			v.t = TypeBool
 			v.v.set(vec.a+uint64(offset), 5)
@@ -128,6 +153,7 @@ func (vec *Vector) parseGeneric(depth, offset int, v *Node) (int, error) {
 	return offset, err
 }
 
+// Object parsing helper.
 func (vec *Vector) parseObj(depth, offset int, v *Node) (int, error) {
 	v.s = vec.r.len(depth)
 	offset++
@@ -141,12 +167,15 @@ func (vec *Vector) parseObj(depth, offset int, v *Node) (int, error) {
 		offset = vec.skipFmt(offset)
 		// Parse key.
 		if vec.s[offset] != '"' {
+			// Key should be a string wrapped with double quotas.
 			return offset, ErrUnexpId
 		}
 		offset++
+		// Register new node.
 		c := vec.newNode(depth)
 		i := vec.l - 1
 		v.e = vec.r.reg(depth, i)
+		// Fill up key's offset and length.
 		c.k.o = vec.a + uint64(offset)
 		e := bytealg.IndexAt(vec.s, bQuote, offset)
 		if e < 0 {
@@ -154,9 +183,11 @@ func (vec *Vector) parseObj(depth, offset int, v *Node) (int, error) {
 		}
 		c.k.e = false
 		if vec.s[e-1] != '\\' {
+			// Key is an unescaped string, good case.
 			c.k.l = e - offset
 			offset = e + 1
 		} else {
+			// Key contains escaped bytes.
 			_ = vec.s[len(vec.s)-1]
 			for i := e; i < len(vec.s); {
 				i = bytealg.IndexAt(vec.s, bQuote, i+1)
@@ -174,28 +205,34 @@ func (vec *Vector) parseObj(depth, offset int, v *Node) (int, error) {
 			offset = e + 1
 		}
 		if !c.k.e {
+			// Extra check of escaped sequences in the key.
 			c.k.e = bytes.IndexByte(c.k.rawBytes(), '\\') >= 0
 		}
 		// Parse value.
 		offset = vec.skipFmt(offset)
+		// Check division symbol.
 		if vec.s[offset] == ':' {
 			offset++
 		} else {
 			return offset, ErrUnexpId
 		}
 		offset = vec.skipFmt(offset)
+		// Value mey be an arbitrary type.
 		offset, err = vec.parseGeneric(depth, offset, c)
 		if err == ErrEOO {
 			err = nil
 			break
 		}
+		// Save node to the vector.
 		vec.v[i] = *c
 		offset = vec.skipFmt(offset)
 		if vec.s[offset] == '}' {
+			// End of the object caught.
 			offset++
 			break
 		}
 		if vec.s[offset] == ',' {
+			// Object elements separator caught.
 			offset++
 		} else {
 			return offset, ErrUnexpId
@@ -205,6 +242,7 @@ func (vec *Vector) parseObj(depth, offset int, v *Node) (int, error) {
 	return offset, err
 }
 
+// Array parsing helper.
 func (vec *Vector) parseArr(depth, offset int, v *Node) (int, error) {
 	v.s = vec.r.len(depth)
 	offset++
@@ -215,21 +253,27 @@ func (vec *Vector) parseArr(depth, offset int, v *Node) (int, error) {
 			offset++
 			break
 		}
+		// Register new node.
 		c := vec.newNode(depth)
 		i := vec.l - 1
 		v.e = vec.r.reg(depth, i)
+		// Parse the value.
 		offset, err = vec.parseGeneric(depth, offset, c)
 		if err == ErrEOA {
+			// End of array isn't the error actually.
 			err = nil
 			break
 		}
+		// Save node to the vector.
 		vec.v[i] = *c
 		offset = vec.skipFmt(offset)
 		if vec.s[offset] == ']' {
+			// End of the array caught.
 			offset++
 			break
 		}
 		if vec.s[offset] == ',' {
+			// Array elements separator caught.
 			offset++
 		} else {
 			return offset, ErrUnexpId
@@ -239,6 +283,9 @@ func (vec *Vector) parseArr(depth, offset int, v *Node) (int, error) {
 	return offset, nil
 }
 
+// Skip formatting symbols like tabs, spaces, ...
+//
+// Returns the next non-format symbol index.
 func (vec *Vector) skipFmt(offset int) int {
 	for bytes.IndexByte(bFmt, vec.s[offset]) != -1 {
 		offset++
@@ -246,10 +293,12 @@ func (vec *Vector) skipFmt(offset int) int {
 	return offset
 }
 
+// Check if given byte is a part of the number.
 func isDigit(c byte) bool {
 	return (c >= '0' && c <= '9') || c == '-' || c == '+' || c == 'e' || c == 'E'
 }
 
+// Check if given is a part of the number, including dot.
 func isDigitDot(c byte) bool {
 	return isDigit(c) || c == '.'
 }
