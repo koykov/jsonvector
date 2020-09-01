d71682e51d6166b21932d35af97d06131376b127
Konstantin Voykov <koykov0@gmail.com>
2020-09-01 23:34:09 +0300

    Comment pool code.

--- pool.go
+++ pool.go
@@ -2,16 +2,20 @@ package jsonvector
 
 import "sync"
 
+// Vector pool.
 type Pool struct {
 	p sync.Pool
 }
 
 var (
+	// Default instance of the pool.
+	// Just call jsonvector.Acquire() and jsonvector.Release().
 	P Pool
-
+	// Suppress go vet warnings.
 	_, _ = Acquire, Release
 )
 
+// Get old vector from the pool or create new one.
 func (p *Pool) Get() *Vector {
 	v := p.p.Get()
 	if v != nil {
@@ -22,15 +26,18 @@ func (p *Pool) Get() *Vector {
 	return NewVector()
 }
 
+// Put vector back to the pool.
 func (p *Pool) Put(vec *Vector) {
 	vec.Reset()
 	p.p.Put(vec)
 }
 
+// Get vector from default pool instance.
 func Acquire() *Vector {
 	return P.Get()
 }
 
+// Put vector back to default pool instance.
 func Release(vec *Vector) {
 	P.Put(vec)
 }
