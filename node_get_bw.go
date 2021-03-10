4af845f4f08476b8e98353ee19459d3562ea6377
Konstantin Voykov <koykov0@gmail.com>
2021-03-10 10:53:29 +0200

    Hotfix node's getters kept for backward compatibility.

--- node_get_bw.go
+++ node_get_bw.go
@@ -4,41 +4,41 @@ package jsonvector
 // Kept for backward compatibility.
 
 // Look and get child object by given path and separator.
-func (n *Node) GetObjectByPath(path string) *Node {
-	return n.GetObjectPS(path, ".")
+func (n *Node) GetObjectByPath(path, sep string) *Node {
+	return n.GetObjectPS(path, sep)
 }
 
 // Look and get child array by given path and separator.
-func (n *Node) GetArrayByPath(path string) *Node {
-	return n.GetArrayPS(path, ".")
+func (n *Node) GetArrayByPath(path, sep string) *Node {
+	return n.GetArrayPS(path, sep)
 }
 
 // Look and get child bytes by given path and separator.
-func (n *Node) GetBytesByPath(path string) []byte {
-	return n.GetBytesPS(path, ".")
+func (n *Node) GetBytesByPath(path, sep string) []byte {
+	return n.GetBytesPS(path, sep)
 }
 
 // Look and get child string by given path and separator.
-func (n *Node) GetStringByPath(path string) string {
-	return n.GetStringPS(path, ".")
+func (n *Node) GetStringByPath(path, sep string) string {
+	return n.GetStringPS(path, sep)
 }
 
 // Look and get child bool by given path and separator.
-func (n *Node) GetBoolByPath(path string) bool {
-	return n.GetBoolPS(path, ".")
+func (n *Node) GetBoolByPath(path, sep string) bool {
+	return n.GetBoolPS(path, sep)
 }
 
 // Look and get child float by given path and separator.
-func (n *Node) GetFloatByPath(path string) (float64, error) {
-	return n.GetFloatPS(path, ".")
+func (n *Node) GetFloatByPath(path, sep string) (float64, error) {
+	return n.GetFloatPS(path, sep)
 }
 
 // Look and get child integer by given path and separator.
-func (n *Node) GetIntByPath(path string) (int64, error) {
-	return n.GetIntPS(path, ".")
+func (n *Node) GetIntByPath(path, sep string) (int64, error) {
+	return n.GetIntPS(path, sep)
 }
 
 // Look and get child unsigned int by given path and separator.
-func (n *Node) GetUintByPath(path string) (uint64, error) {
-	return n.GetUintPS(path, ".")
+func (n *Node) GetUintByPath(path, sep string) (uint64, error) {
+	return n.GetUintPS(path, sep)
 }
