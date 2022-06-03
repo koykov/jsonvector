d2f61e3076481beb63e95028dc6cb7e74ea227b2
Konstantin Voykov <koykov0@gmail.com>
2022-06-03 23:50:03 +0300

    Fix linter comments.

--- vector.go
+++ vector.go
@@ -7,12 +7,12 @@ import (
 	"github.com/koykov/vector"
 )
 
-// Parser object.
+// Vector represents URL parser.
 type Vector struct {
 	vector.Vector
 }
 
-// Make new parser.
+// NewVector makes new parser.
 func NewVector() *Vector {
 	vec := &Vector{}
 	vec.Helper = helper
@@ -24,22 +24,22 @@ func (vec *Vector) Parse(s []byte) error {
 	return vec.parse(s, false)
 }
 
-// Parse source string.
+// ParseStr parses source string.
 func (vec *Vector) ParseStr(s string) error {
 	return vec.parse(fastconv.S2B(s), false)
 }
 
-// Copy source bytes and parse it.
+// ParseCopy copies source bytes and parse it.
 func (vec *Vector) ParseCopy(s []byte) error {
 	return vec.parse(s, true)
 }
 
-// Copy source string and parse it.
+// ParseCopyStr copies source string and parse it.
 func (vec *Vector) ParseCopyStr(s string) error {
 	return vec.parse(fastconv.S2B(s), true)
 }
 
-// Format vector in human readable representation.
+// Beautify formats vector in human-readable representation.
 func (vec *Vector) Beautify(w io.Writer) error {
 	r := vec.Root()
 	return vec.beautify(w, r, 0)
