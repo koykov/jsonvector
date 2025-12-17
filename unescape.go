598831da73fabce495b9822f9d70059085cf8b4e
Konstantin Voykov <koykov0@gmail.com>
2025-12-17 22:59:19 +0300

    parse uint optimization

--- unescape.go
+++ unescape.go
@@ -1,10 +1,8 @@
 package jsonvector
 
 import (
-	"strconv"
 	"unicode/utf16"
 
-	"github.com/koykov/byteconv"
 	"github.com/koykov/simd/indexbyte"
 )
 
@@ -51,11 +49,7 @@ func Unescape(p []byte) []byte {
 				continue
 			}
 			x := p[i+2 : i+6]
-			u, err := strconv.ParseUint(byteconv.B2S(x), 16, 16)
-			if err != nil {
-				i++
-				continue
-			}
+			u := xtouTable(x)
 			r := rune(u)
 			if !utf16.IsSurrogate(r) {
 				// Regular utf8 symbol.
@@ -76,11 +70,7 @@ func Unescape(p []byte) []byte {
 					continue
 				} else {
 					x = p[i+8 : i+12]
-					u1, err := strconv.ParseUint(byteconv.B2S(x), 16, 16)
-					if err != nil {
-						i++
-						continue
-					}
+					u1 := xtouTable(x)
 					r = utf16.DecodeRune(r, rune(u1))
 					s := string(r)
 					z := len(s)
