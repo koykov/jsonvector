37cd54191fe3f1e11e2d9812fea2ebbfd705b907
Konstantin Voykov <koykov0@gmail.com>
2026-03-18 23:34:18 +0300

    multiroot: prepare testing

---
+++ multiroot_test.go
@@ -0,0 +1,67 @@
+package jsonvector
+
+import (
+	"math/rand"
+	"os"
+	"path/filepath"
+	"strconv"
+	"testing"
+)
+
+type multirootStage struct {
+	key string
+	buf []byte
+}
+
+var (
+	multiroots [][]byte
+
+	multirootStages    []multirootStage
+	multirootStagesReg = map[string]int{}
+)
+
+func init() {
+	_ = filepath.Walk("testdata/multiroot", func(path string, info os.FileInfo, err error) error {
+		if filepath.Ext(path) == ".json" {
+			b, _ := os.ReadFile(path)
+			multiroots = append(multiroots, b)
+			return nil
+		}
+		return nil
+	})
+	for i := 10; i <= 100; i += 10 {
+		key := strconv.Itoa(i)
+		st := multirootStage{key: key}
+		for j := 0; j < i; j++ {
+			st.buf = append(st.buf, multiroots[j%len(multiroots)]...)
+			if j > 0 {
+				switch rand.Intn(5) {
+				case 0:
+					st.buf = append(st.buf, '\n')
+				case 1:
+					st.buf = append(st.buf, '\r')
+				case 2:
+					st.buf = append(st.buf, '\t')
+				case 3:
+					st.buf = append(st.buf, ' ')
+				case 4:
+					st.buf = append(st.buf, "\n\r\n\n\r\r\t\t   "...)
+				}
+			}
+		}
+		multirootStages = append(multirootStages, st)
+		multirootStagesReg[key] = len(multirootStages) - 1
+	}
+}
+
+func TestMultiroot(t *testing.T) {
+	for i := 10; i <= 100; i += 10 {
+		key := strconv.Itoa(i)
+		t.Run(key, func(t *testing.T) {
+			idx := multirootStagesReg[key]
+			st := &multirootStages[idx]
+			t.Log(key)
+			_ = st
+		})
+	}
+}
